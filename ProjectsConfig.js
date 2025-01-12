c7a5bdff552d94eb69ae68d35056e0e306eb62e7
ms7ru <rhythm.upa@gmail.com>
2025-01-12 14:28:44 +0530

    dssd

--- ProjectsConfig.js
+++ ProjectsConfig.js
@@ -3,24 +3,30 @@ var ConfigForProjectsInHtml = {
         image: "https://cloud-qiuj6dsav-hack-club-bot.vercel.app/0image.png",
         Titel: "This website",
         discription: "Cool portifolio with lots of animation displaying my projects",
-        link: "/index.html"
+        link: "./index.html"
     },
     2: {
         image: "https://cloud-fphg73xlv-hack-club-bot.vercel.app/0image.png",
         Titel: "A New Tab",
         discription: "A project to enhance your browser's new tab functionality.",
-        link: "https://ms7ru.github.io/NEW-TAB/"
+        link: "https://nt.atb.quest"
     },
     3: {
         image: "https://cloud-pv4hlfur2-hack-club-bot.vercel.app/0image.png",
         Titel: "No One Can See You",
         discription: "'No One Can See You' is a thrilling story game where every choice matters.",
-        link: "https://ms7ru.github.io/No-One-Can-See-You/"
+        link: "https://nope.atb.quest"
     },
     4: {
         image: "https://cloud-r1s893lpi-hack-club-bot.vercel.app/0image.png",
         Titel: "Party Countdown Website",
         discription: "A dynamic and interactive birthday party countdown website.",
-        link: "https://ms7ru.github.io/EVENT-COUNDOWN/"
+        link: "https://et.atb.quest"
+    },
+    5: {
+        image : "https://cloud-m3v0wxjgy-hack-club-bot.vercel.app/0image.png",
+        Titel : "Better Github",
+        discription: "A dynamic and interactive birthday party countdown website.",
+        link: "https://et.atb.quest"
     }
 }
